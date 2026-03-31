1793662c0836a921c75afcddbd1f965b28425a25
workingdog <ringowathelet@gmail.com>
2026-03-31 13:37:10 +0900

    Initial Commit

---
+++ VisNet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VisNet
+//
+//  Created by Ringo Wathelet on 2026/03/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisNet/VisNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisNetApp.swift
+//  VisNet
+//
+//  Created by Ringo Wathelet on 2026/03/31.
+//
+
+import SwiftUI
+
+@main
+struct VisNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
