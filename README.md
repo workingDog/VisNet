256abc6ad5f1237800b7c524b331e3401d44d950
workingdog <ringowathelet@gmail.com>
2026-03-31 15:50:00 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# SwiftUI Network visualization
+
+Some basic code using the javascript visualization library [vis.js community edition](https://visjs.org/) in a SwiftUI app using a WKWebView.
+
+See 
+
+[vis.js community edition](https://visjs.org/), "A dynamic, browser based visualization library.
+The library is designed to be easy to use, to handle large amounts of dynamic data, and to enable manipulation of and interaction with the data.
+The library consists of the components DataSet, Timeline, Network, Graph2d and Graph3d."
+
+On github, [vis.js](https://github.com/visjs) "Dynamic, browser based visualization libraries".
+
+Specifically [vis-network](https://github.com/visjs/vis-network) a basic network example.
+
+<p float="left">
+  <img src="picture1.png" width="400" height="400" />
+</p>
--- VisNet/ContentView.swift
+++ VisNet/ContentView.swift
@@ -4,21 +4,35 @@
 //
 //  Created by Ringo Wathelet on 2026/03/31.
 //
-
 import SwiftUI
+import WebKit
+
 
 struct ContentView: View {
+
+    @State private var nodes = [
+        ["id": 1, "label": "Node 1"],
+        ["id": 2, "label": "Node 2"]
+    ]
+
+    @State private var edges = [ ["from": 1, "to": 2] ]
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            VisNetworkView(nodesJSON: json(nodes), edgesJSON: json(edges)) { message in
+                print("---> clicked: ", message)
+            }.frame(height: 400)
 
-#Preview {
-    ContentView()
+            Button("Add Node") {
+                let id = nodes.count + 1
+                nodes.append(["id": id, "label": "Node \(id)"])
+                edges.append(["from": 1, "to": id])
+            }.buttonStyle(.borderedProminent)
+        }
+    }
+
+    private func json(_ obj: Any) -> String {
+        let data = try! JSONSerialization.data(withJSONObject: obj)
+        return String(data: data, encoding: .utf8)!
+    }
 }
